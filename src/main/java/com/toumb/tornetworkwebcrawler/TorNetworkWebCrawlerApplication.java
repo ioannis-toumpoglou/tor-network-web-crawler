33a1ffca49db69000990b321aa7114a175a360c7
Ioannis Toumpoglou <ioannis.toumpoglou@gmail.com>
2021-03-20 20:06:50 +0200

    Init project

---
+++ src/main/java/com/toumb/tornetworkwebcrawler/TorNetworkWebCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.toumb.tornetworkwebcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorNetworkWebCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorNetworkWebCrawlerApplication.class, args);
+	}
+
+}
