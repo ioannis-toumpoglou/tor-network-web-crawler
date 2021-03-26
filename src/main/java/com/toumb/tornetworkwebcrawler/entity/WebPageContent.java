c441738e35c86314ade9f47117926da0637b2c26
ioannis-toumpoglou <ioannis.toumpoglou@gmail.com>
2021-03-26 18:19:44 +0200

    Content and Threat Type relationship fix

--- src/main/java/com/toumb/tornetworkwebcrawler/entity/WebPageContent.java
+++ src/main/java/com/toumb/tornetworkwebcrawler/entity/WebPageContent.java
@@ -7,7 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -26,7 +26,7 @@ public class WebPageContent {
 	@Column(name="url")
 	private String url;
 	
-	@OneToOne(cascade = CascadeType.ALL)
+	@ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "threat_type_id", referencedColumnName = "id")
 	private ThreatType threatType;
 	
