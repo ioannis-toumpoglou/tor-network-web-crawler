0010bc32d4febfabc900e1ee0515b26aaa7f5436
Ioannis Toumpoglou <ioannis.toumpoglou@gmail.com>
2021-03-28 15:34:51 +0300

    Tor Network Url service implementation

---
+++ src/main/java/com/toumb/tornetworkwebcrawler/service/TorNetworkUrlService.java
@@ -0,0 +1,17 @@
+package com.toumb.tornetworkwebcrawler.service;
+
+import java.util.List;
+
+import com.toumb.tornetworkwebcrawler.entity.TorNetworkUrl;
+
+public interface TorNetworkUrlService {
+	
+	public List<TorNetworkUrl> findAll();
+	
+	public TorNetworkUrl findById(int id);
+	
+	public void save(TorNetworkUrl torNetorkUrl);
+	
+	public void deleteById(int id);
+
+}
---
+++ src/main/java/com/toumb/tornetworkwebcrawler/service/TorNetworkUrlServiceImpl.java
@@ -0,0 +1,52 @@
+package com.toumb.tornetworkwebcrawler.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.toumb.tornetworkwebcrawler.dao.TorNetworkUrlRepository;
+import com.toumb.tornetworkwebcrawler.entity.TorNetworkUrl;
+
+@Service
+public class TorNetworkUrlServiceImpl implements TorNetworkUrlService {
+
+	private TorNetworkUrlRepository torNetworkUrlRepository;
+	
+	@Autowired
+	public TorNetworkUrlServiceImpl(TorNetworkUrlRepository theTorNetworkUrlRepository) {
+		torNetworkUrlRepository = theTorNetworkUrlRepository;
+	}
+	
+	@Override
+	public List<TorNetworkUrl> findAll() {
+		return torNetworkUrlRepository.findAllByOrderByUrlAsc();
+	}
+
+	@Override
+	public TorNetworkUrl findById(int id) {
+		Optional<TorNetworkUrl> result = torNetworkUrlRepository.findById(id);
+		
+		TorNetworkUrl torNetworkUrl = null;
+		// Check if the url exists
+		if (result.isPresent()) {
+			torNetworkUrl = result.get();
+		} else {
+			throw new RuntimeException("Unable to find the url id - " + id);
+		}
+		
+		return torNetworkUrl;
+	}
+
+	@Override
+	public void save(TorNetworkUrl torNetworkUrl) {
+		torNetworkUrlRepository.save(torNetworkUrl);
+	}
+
+	@Override
+	public void deleteById(int id) {		
+		torNetworkUrlRepository.deleteById(id);
+	}
+
+}
