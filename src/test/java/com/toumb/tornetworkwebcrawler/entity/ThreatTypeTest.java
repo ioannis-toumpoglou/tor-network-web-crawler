965a9faeb559c166f60a3e5e72513fc9a5a29b99
ioannis-toumpoglou <ioannis.toumpoglou@gmail.com>
2021-03-30 17:52:36 +0300

    Threat Type tests

--- src/test/java/com/toumb/tornetworkwebcrawler/entity/ThreatTypeTest.java
+++ src/test/java/com/toumb/tornetworkwebcrawler/entity/ThreatTypeTest.java
@@ -9,12 +9,15 @@ public class ThreatTypeTest {
 	
 	@Test
 	void getIdTest() {
+		// Manually set ID
 		ThreatType threat = new ThreatType();
 		threat.setId(1);
 		int id = threat.getId();
 		assertEquals(1, id);
 		
-		ThreatType secondThreat = new ThreatType(2, "Spam");
+		// ID set in constructor
+		ThreatType secondThreat = new ThreatType(2, "Spam", "Irrelevant or unsolicited messages sent over the internet, "
+				+ "typically to a large number of users, for the purposes of advertising, phishing, spreading malware, etc.");
 		int secondId = secondThreat.getId();
 		assertEquals(2, secondId);
 		assertNotEquals(3, secondId);
@@ -22,16 +25,39 @@ public class ThreatTypeTest {
 	
 	@Test
 	void getThreatTypeTest() {
+		// Manually set type
 		ThreatType threat = new ThreatType();
 		threat.setTypeName("Malware");
 		String type = threat.getTypeName();
 		assertEquals("Malware", type);
 		assertNotEquals("Spam", type);
 		
-		ThreatType secondThreat = new ThreatType(2, "Spam");
+		// Type set in constructor
+		ThreatType secondThreat = new ThreatType(2, "Spam", "Irrelevant or unsolicited messages sent over the internet, "
+				+ "typically to a large number of users, for the purposes of advertising, phishing, spreading malware, etc.");
 		String secondType = secondThreat.getTypeName();
 		assertEquals("Spam", secondType);
 		assertNotEquals("Malware", secondType);
+	}
+	
+	@Test
+	void getThreatDescriptionTest() {
+		// Manually set description
+		ThreatType threat = new ThreatType();
+		threat.setTypeName("Malware");
+		threat.setDescription("Irrelevant or unsolicited messages sent over the internet, typically to a large number of users, "
+				+ "for the purposes of advertising, phishing, spreading malware, etc.");
+		String threatDescription = threat.getDescription();
+		assertEquals("Irrelevant or unsolicited messages sent over the internet, typically to a large number of users, "
+				+ "for the purposes of advertising, phishing, spreading malware, etc.", threatDescription);
+		
+		// Description set in constructor
+		ThreatType secondThreat = new ThreatType(2, "Spam", "Irrelevant or unsolicited messages sent over the internet, "
+				+ "typically to a large number of users, for the purposes of advertising, phishing, spreading malware, etc.");
+		String secondThreatDescription = secondThreat.getDescription();
+		assertEquals("Irrelevant or unsolicited messages sent over the internet, typically to a large number of users, "
+				+ "for the purposes of advertising, phishing, spreading malware, etc.", secondThreatDescription);
+		assertNotEquals("Malware", secondThreatDescription);
 	}
 	
 }
